afd1f1122d6d0c1ace9f24ea09a1935e7aa01e6f
Jurian <JurianVanDerWoude@gmail.com>
2023-04-05 22:55:40 +0200

    doelstelling_9

--- Bash/mijnbash.sh
+++ Bash/mijnbash.sh
@@ -1,28 +1,44 @@
 #! /bin/bash
 
+# check dat er niet teveel of te weinig argumenten zijn meegegeven 
 if [ "$#" -lt 1 ]; then
-	echo "Please add one or two arguments."
+	echo "Geef tenminste het bestandsnaam mee als argument en eventueel daarna uw landcode."
 	exit 2
 elif [ "$#" -gt 2 ]; then
-	echo "Please add only one or two arguments. Too many arguments were added."
+	echo "U heeft teveel argumenten gegeven."
 	exit 2
 fi
 
+# haal het bestand op in de records folder
 file=$( grep -e . "records\\$1" );
+# check of de vorige command goed uitgevoerd is en anders stop 
 if [ "$?" != 0 ]; then
-	echo "File could not be found"
+	echo "We konden het bestand niet vinden in \"records\". 
+		Voeg aub het juiste bestand als eerste parameter toe."
 	exit
+# als het bestand bestaat laat dan de inhoud zien 
 elif [ -n "$file" ]; then 
 	echo "Wanneer we \"$1\" openen zien we het volgende:"
 	echo "$file"
 fi
 
-if [ "$#" -eq 2 ] && [ "$2" == "w" ]; then
-	echo "Wil je een nummer hieraan toevoegen?"
-	read nummer 
-	echo "$nummer" >> "records\\$1"
-	echo "Het nummer $nummer is toegevoegd aan \"$1\"."
+landcode="+31"
+netnummer=""
+
+# geef het netnummer mee als er twee parameters zijn en
+# als het netnummer bestaat uit 0xx of 0xxx
+if [ "$#" -eq 2 ] && [[ $2 =~ 0[0-9]{2,3} ]]; then 
+	# zet het netnummer gelijke aan de tweede parameter
+	netnummer="$2"
+	# haal de eerste nul van het netnummer
+	netnummer=${netnummer:1}
 fi
 
 
+echo "We gaan nu \"$landcode""$netnummer\" toevoegen aan alle nummers."
 
+# leest de records regel voor regel en voegt het toe aan een nieuwe file
+cat records\\$1 | while read line; do
+	echo "$line wordt veranderd in $landcode""$netnummer""$line"
+	echo "$landcode""$netnummer""$line" >> "newfile.txt"
+done
